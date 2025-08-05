4b78f9476def14d290d7590bbcaf83d72397a3b5
F1breXW <1509385773@qq.com>
2025-08-05 13:54:57 +0800

    room_management_module上传

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -1,5 +1,29 @@
+// 员工信息表 (StaffInfo)
 public DbSet<StaffInfo> StaffInfos { get; set; }
+
+// 老人信息表 (ElderInfo)
 public DbSet<ElderlyInfo> ElderlyInfos { get; set; }
+
+// 家属信息表 (FamilyInfo)
 public DbSet<FamilyInfo> FamilyInfos { get; set; }
+
+// 护理计划表 (NursingPlan)
 public DbSet<NursingPlan> NursingPlans { get; set; }
+
+// 费用结算表 (FeeSettlement)
 public DbSet<FeeSettlement> FeeSettlements { get; set; }
+
+// 健康监测记录表 (HealthMonitoring)
+public DbSet<HealthMonitoring> HealthMonitorings { get; set; }
+
+// 房间管理表 (RoomManagement)
+public DbSet<RoomManagement> RoomManagements { get; set; }
+
+// 设备状态表 (DeviceStatus)
+public DbSet<DeviceStatus> DeviceStatuses { get; set; }
+
+// 电子围栏表 (ElectronicFence)
+public DbSet<ElectronicFence> ElectronicFences { get; set; }
+
+// 围栏出入记录表 (FenceLog)
+public DbSet<FenceLog> FenceLogs { get; set; }
