f590966a70e300b244fce5aa21b08f3aca41bdd0
魏同烜 <161698040+Changgrette@users.noreply.github.com>
2025-08-08 10:44:30 +0800

    Update EmergencySOS.cs

--- Data/Entities/Modules/StaffInfo_Module/EmergencySOS.cs
+++ Data/Entities/Modules/StaffInfo_Module/EmergencySOS.cs
@@ -1,11 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Staff_Info.Models;
 
 public class EMERGENCYSOS
 {
-    [Key]public decimal CALL_ID { get; set; }
+    [Key]
+    [Column("CALL_ID")]
+    public decimal CALL_ID { get; set; }  // 改为int以匹配NUMBER
+    
+    [ForeignKey("ELDERLY")]  // This links to the navigation property
+    [Column("ELDERLY_ID")]   // Explicit column name
     public decimal ELDERLY_ID { get; set; }
+    
     public DateTime CALL_TIME { get; set; }
     public string CALL_TYPE { get; set; }
     public decimal? ROOM_ID { get; set; }
@@ -17,7 +24,10 @@ public class EMERGENCYSOS
 
     // 外键导航属性
     public ELDERLYINFO ELDERLY { get; set; }
+    
+    [ForeignKey("ROOM_ID")]
     public ROOMMANAGEMENT ROOM { get; set; }
+    
+    [ForeignKey("RESPONSE_STAFF_ID")]
     public STAFFINFO RESPONSE_STAFF { get; set; }
-
 }
