0e7743ca7c71705de3c6e1dbf7d2de37b5f0aa46
F1breXW <1509385773@qq.com>
2025-09-14 12:28:01 +0800

    部署包发布

--- obj/Debug/net8.0/RoomDeviceManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RoomDeviceManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoomDeviceManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae821254e61216e2437ebe8757ae004d7131f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ac41fb3bcc9f613e5e292346a81fa17352437b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoomDeviceManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoomDeviceManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
